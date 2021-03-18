690408fa4fdc809589f78ba64597f12bb04bb689
Beyonce Knowles <you@example.com>
2021-03-18 17:36:24 -0400

    typo fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || '
-  postgres://ncptmxdenhqoyf:deed7d9a49533e3f62c10daa3df1d1b2fd24eeb753cb94f2a397a615ff19e741@ec2-52-7-115-250.compute-1.amazonaws.com:5432/dfb22loru6ore2',
+  DATABASE_URL: process.env.DB_URL || 'postgres://ncptmxdenhqoyf:deed7d9a49533e3f62c10daa3df1d1b2fd24eeb753cb94f2a397a615ff19e741@ec2-52-7-115-250.compute-1.amazonaws.com:5432/dfb22loru6ore2',
   TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin:thinkful2020@localhost/retrotest',
 };
