45d311b2c317df83db08862e10a7e92b65501995
Beyonce Knowles <you@example.com>
2021-03-17 23:26:50 -0400

    db to database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:thinkful2020@localhost/retro',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin:thinkful2020@localhost/retrotest',
+  DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:thinkful2020@localhost/retro',
+  TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin:thinkful2020@localhost/retrotest',
 };
